7fe1446d57c256decb31831f22fe3b4eabafa4c2
gabelchinmay <chinmay.gabel@spectrocloud.com>
2024-07-18 01:41:24 -0700

    code review: add yaml next to json tag
    Signed-off-by: gabelchinmay <chinmay.gabel@spectrocloud.com>

--- schema/install_schema.go
+++ schema/install_schema.go
@@ -7,44 +7,44 @@ import (
 // InstallSchema represents the install block in the Kairos configuration. It is used to drive automatic installations without user interaction.
 type InstallSchema struct {
 	_                   struct{}       `title:"Kairos Schema: Install block" description:"The install block is to drive automatic installations without user interaction."`
-	Auto                bool           `json:"auto,omitempty" description:"Set to true when installing without Pairing" yaml:"auto,omitempty"`
+	Auto                bool           `json:"auto,omitempty" yaml:"auto,omitempty" description:"Set to true when installing without Pairing"`
 	BindMounts          []string       `json:"bind_mounts,omitempty" yaml:"bind_mounts,omitempty"`
-	Bundles             []BundleSchema `json:"bundles,omitempty" description:"Add bundles in runtime" yaml:"bundles,omitempty"`
+	Bundles             []BundleSchema `json:"bundles,omitempty" yaml:"bundles,omitempty" description:"Add bundles in runtime"`
 	NoFormat            bool           `json:"no_format,omitempty" yaml:"no-format,omitempty"`
-	Device              string         `json:"device,omitempty" pattern:"^(auto|/|(/[a-zA-Z0-9_-]+)+)$" description:"Device for automated installs" examples:"[\"auto\",\"/dev/sda\"]" yaml:"device,omitempty"`
+	Device              string         `json:"device,omitempty" yaml:"device,omitempty" pattern:"^(auto|/|(/[a-zA-Z0-9_-]+)+)$" description:"Device for automated installs" examples:"[\"auto\",\"/dev/sda\"]"`
 	EphemeralMounts     []string       `json:"ephemeral_mounts,omitempty" yaml:"ephemeral_mounts,omitempty"`
 	EncryptedPartitions []string       `json:"encrypted_partitions,omitempty" yaml:"encrypted_partitions,omitempty"`
 	Env                 []interface{}  `json:"env,omitempty" yaml:"env,omitempty"`
 	GrubOptionsSchema   `json:"grub_options,omitempty" yaml:"grub_options,omitempty"`
-	Image               string `json:"image,omitempty" description:"Use a different container image for the installation" yaml:"image,omitempty"`
+	Image               string `json:"image,omitempty" yaml:"image,omitempty" description:"Use a different container image for the installation"`
 	PowerManagement
 	SkipEncryptCopyPlugins bool                `json:"skip_copy_kcrypt_plugin,omitempty" yaml:"skip_copy_kcrypt_plugin,omitempty"`
-	Partitions             ElementalPartitions `json:"partitions,omitempty" mapstructure:"partitions" yaml:"partitions,omitempty"`
-	GrubDefEntry           string              `json:"grub-entry-name,omitempty" mapstructure:"grub-entry-name" yaml:"grub-entry-name,omitempty"`
-	ExtraPartitions        []*Partition        `json:"extra-partitions,omitempty" mapstructure:"extra-partitions" yaml:"extra-partitions,omitempty"`
-	Force                  bool                `json:"force,omitempty" mapstructure:"force" yaml:"force,omitempty"`
+	Partitions             ElementalPartitions `json:"partitions,omitempty" yaml:"partitions,omitempty" mapstructure:"partitions"`
+	GrubDefEntry           string              `json:"grub-entry-name,omitempty" yaml:"grub-entry-name,omitempty" mapstructure:"grub-entry-name"`
+	ExtraPartitions        []*Partition        `json:"extra-partitions,omitempty" yaml:"extra-partitions,omitempty" mapstructure:"extra-partitions"`
+	Force                  bool                `json:"force,omitempty" yaml:"force,omitempty" mapstructure:"force"`
 	ExtraDirsRootfs        []string            `json:"extra-dirs-rootfs,omitempty" yaml:"extra-dirs-rootfs,omitempty" mapstructure:"extra-dirs-rootfs"`
-	Active                 Image               `json:"system,omitempty" mapstructure:"system" yaml:"system,omitempty"`
-	Recovery               Image               `json:"recovery-system,omitempty" mapstructure:"recovery-system" yaml:"recovery-system,omitempty"`
-	Passive                Image               `json:"passive,omitempty" mapstructure:"recovery-system" yaml:"passive,omitempty"`
+	Active                 Image               `json:"system,omitempty" yaml:"system,omitempty" mapstructure:"system"`
+	Recovery               Image               `json:"recovery-system,omitempty" yaml:"recovery-system,omitempty" mapstructure:"recovery-system"`
+	Passive                Image               `json:"passive,omitempty" yaml:"passive,omitempty" mapstructure:"recovery-system"`
 }
 
 type Image struct {
-	Size   uint   `json:"size,omitempty" mapstructure:"size" yaml:"size,omitempty"`
-	Source string `json:"uri,omitempty" mapstructure:"uri" yaml:"uri,omitempty"`
+	Size   uint   `json:"size,omitempty" yaml:"size,omitempty" mapstructure:"size"`
+	Source string `json:"uri,omitempty" yaml:"uri,omitempty" mapstructure:"uri"`
 }
 
 type Partition struct {
 	Name string `json:"name,omitempty" yaml:"name,omitempty"`
-	Size uint   `json:"size,omitempty" mapstructure:"size" yaml:"size,omitempty"`
-	FS   string `json:"fs,omitempty" mapstrcuture:"fs" yaml:"fs,omitempty"`
+	Size uint   `json:"size,omitempty" yaml:"size,omitempty" mapstructure:"size"`
+	FS   string `json:"fs,omitempty" yaml:"fs,omitempty" mapstrcuture:"fs"`
 }
 
 type ElementalPartitions struct {
-	OEM        *Partition `json:"oem,omitempty" mapstructure:"oem" yaml:"oem,omitempty"`
-	Recovery   *Partition `json:"recovery,omitempty" mapstructure:"recovery" yaml:"recovery,omitempty"`
-	State      *Partition `json:"state,omitempty" mapstructure:"state" yaml:"state,omitempty"`
-	Persistent *Partition `json:"persistent,omitempty" mapstructure:"persistent" yaml:"persistent,omitempty"`
+	OEM        *Partition `json:"oem,omitempty" yaml:"oem,omitempty" mapstructure:"oem"`
+	Recovery   *Partition `json:"recovery,omitempty" yaml:"recovery,omitempty" mapstructure:"recovery"`
+	State      *Partition `json:"state,omitempty" yaml:"state,omitempty" mapstructure:"state"`
+	Persistent *Partition `json:"persistent,omitempty" yaml:"persistent,omitempty" mapstructure:"persistent"`
 }
 
 // BundleSchema represents the bundle block which can be used in different places of the Kairos configuration. It is used to reference a bundle and its confguration.
@@ -58,14 +58,14 @@ type BundleSchema struct {
 
 // GrubOptionsSchema represents the grub options block which can be used in different places of the Kairos configuration. It is used to configure grub.
 type GrubOptionsSchema struct {
-	DefaultFallback      string `json:"default_fallback,omitempty" description:"Sets default fallback logic" yaml:"default_fallback,omitempty"`
-	DefaultMenuEntry     string `json:"default_menu_entry,omitempty" description:"Change GRUB menu entry" yaml:"default_menu_entry,omitempty"`
-	ExtraActiveCmdline   string `json:"extra_active_cmdline,omitempty" description:"Additional Kernel option cmdline to apply just for active" yaml:"extra_active_cmdline,omitempty"`
-	ExtraCmdline         string `json:"extra_cmdline,omitempty" description:"Additional Kernel option cmdline to apply" yaml:"extra_cmdline,omitempty"`
-	ExtraPassiveCmdline  string `json:"extra_passive_cmdline,omitempty" description:"Additional Kernel option cmdline to apply just for passive" yaml:"extra_passive_cmdline,omitempty"`
-	ExtraRecoveryCmdline string `json:"extra_recovery_cmdline,omitempty" description:"Set additional boot commands when booting into recovery" yaml:"extra_recovery_cmdline,omitempty"`
-	NextEntry            string `json:"next_entry,omitempty" description:"Set the next reboot entry." yaml:"next_entry,omitempty"`
-	SavedEntry           string `json:"saved_entry,omitempty" description:"Set the default boot entry." yaml:"saved_entry,omitempty"`
+	DefaultFallback      string `json:"default_fallback,omitempty" yaml:"default_fallback,omitempty" description:"Sets default fallback logic"`
+	DefaultMenuEntry     string `json:"default_menu_entry,omitempty" yaml:"default_menu_entry,omitempty" description:"Change GRUB menu entry"`
+	ExtraActiveCmdline   string `json:"extra_active_cmdline,omitempty" yaml:"extra_active_cmdline,omitempty" description:"Additional Kernel option cmdline to apply just for active"`
+	ExtraCmdline         string `json:"extra_cmdline,omitempty" yaml:"extra_cmdline,omitempty" description:"Additional Kernel option cmdline to apply"`
+	ExtraPassiveCmdline  string `json:"extra_passive_cmdline,omitempty" yaml:"extra_passive_cmdline,omitempty" description:"Additional Kernel option cmdline to apply just for passive"`
+	ExtraRecoveryCmdline string `json:"extra_recovery_cmdline,omitempty" yaml:"extra_recovery_cmdline,omitempty" description:"Set additional boot commands when booting into recovery"`
+	NextEntry            string `json:"next_entry,omitempty" yaml:"next_entry,omitempty" description:"Set the next reboot entry."`
+	SavedEntry           string `json:"saved_entry,omitempty" yaml:"saved_entry,omitempty" description:"Set the default boot entry."`
 }
 
 // PowerManagement is a meta structure to hold the different rules for managing power, which are not compatible between each other.
@@ -74,20 +74,20 @@ type PowerManagement struct {
 
 // NoPowerManagement is a meta structure used when the user does not define any power management options or when the user does not want to reboot or poweroff the machine.
 type NoPowerManagement struct {
-	Reboot   bool `json:"reboot,omitempty" const:"false" default:"false" description:"Reboot after installation" yaml:"reboot,omitempty"`
-	Poweroff bool `json:"poweroff,omitempty" const:"false" default:"false" description:"Power off after installation" yaml:"poweroff,omitempty"`
+	Reboot   bool `json:"reboot,omitempty" yaml:"reboot,omitempty" const:"false" default:"false" description:"Reboot after installation"`
+	Poweroff bool `json:"poweroff,omitempty" yaml:"poweroff,omitempty" const:"false" default:"false" description:"Power off after installation"`
 }
 
 // RebootOnly is a meta structure used to enforce that when the reboot option is set, the poweroff option is not set.
 type RebootOnly struct {
-	Reboot   bool `json:"reboot,omitempty" const:"true" default:"false" required:"true" description:"Reboot after installation" yaml:"reboot,omitempty"`
-	Poweroff bool `json:"poweroff,omitempty" const:"false" default:"false" description:"Power off after installation" yaml:"poweroff,omitempty"`
+	Reboot   bool `json:"reboot,omitempty" yaml:"reboot,omitempty" const:"true" default:"false" required:"true" description:"Reboot after installation"`
+	Poweroff bool `json:"poweroff,omitempty" yaml:"poweroff,omitempty" const:"false" default:"false" description:"Power off after installation"`
 }
 
 // PowerOffOnly is a meta structure used to enforce that when the poweroff option is set, the reboot option is not set.
 type PowerOffOnly struct {
-	Reboot   bool `json:"reboot,omitempty" const:"false" default:"false" description:"Reboot after installation" yaml:"reboot,omitempty"`
-	Poweroff bool `json:"poweroff,omitempty" const:"true" default:"false" required:"true" description:"Power off after installation" yaml:"poweroff,omitempty"`
+	Reboot   bool `json:"reboot,omitempty" yaml:"reboot,omitempty" const:"false" default:"false" description:"Reboot after installation"`
+	Poweroff bool `json:"poweroff,omitempty" yaml:"poweroff,omitempty" const:"true" default:"false" required:"true" description:"Power off after installation"`
 }
 
 var _ jsonschemago.OneOfExposer = PowerManagement{}
