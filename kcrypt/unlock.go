862ff8fb14c32bc88b3ada562d1e80282f6ddf35
Dimitris Karakasilis <dimitris@karakasilis.me>
2025-10-30 16:06:54 +0200

    Move functions to methods of the Encryptor
    because they are only used there and this simplifies their signature
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- kcrypt/unlock.go
+++ kcrypt/unlock.go
@@ -8,70 +8,10 @@ import (
 	"time"
 
 	"github.com/anatol/luks.go"
-	"github.com/jaypipes/ghw/pkg/block"
-	"github.com/kairos-io/kairos-sdk/kcrypt/bus"
 	"github.com/kairos-io/kairos-sdk/types"
 	"github.com/kairos-io/kairos-sdk/utils"
-	"github.com/mudler/go-pluggable"
 )
 
-// getPassword gets the password for a block.Partition using KcryptConfig.
-// It constructs the DiscoveryPasswordPayload internally for communication with the plugin.
-// TODO: Ask to discovery a pass to unlock. keep waiting until we get it and a timeout is exhausted with retrials (exp backoff).
-func getPassword(b *block.Partition, kcryptConfig *bus.KcryptConfig) (password string, err error) {
-	// Get a logger for debugging
-	log := types.NewKairosLogger("kcrypt-getPassword", "info", false)
-	defer log.Close()
-
-	log.Logger.Info().
-		Str("partition_name", b.Name).
-		Str("partition_label", b.FilesystemLabel).
-		Str("partition_uuid", b.UUID).
-		Msg("Requesting password for partition")
-
-	bus.Reload()
-
-	bus.Manager.Response(bus.EventDiscoveryPassword, func(_ *pluggable.Plugin, r *pluggable.EventResponse) {
-		password = r.Data
-		if r.Errored() {
-			err = fmt.Errorf("failed discovery: %s", r.Error)
-			log.Logger.Error().Err(err).Msg("Plugin returned error")
-		} else {
-			log.Logger.Info().
-				Int("password_length", len(password)).
-				Str("partition", b.Name).
-				Msg("DECRYPTION: Received password from plugin")
-		}
-	})
-
-	// Construct DiscoveryPasswordPayload from KcryptConfig + partition
-	// This is where we create the payload that will be sent to kcrypt-challenger
-	var payload bus.DiscoveryPasswordPayload
-	payload.Partition = b
-	if kcryptConfig != nil {
-		payload.ChallengerServer = kcryptConfig.ChallengerServer
-		payload.MDNS = kcryptConfig.MDNS
-		log.Logger.Info().
-			Str("challenger_server", payload.ChallengerServer).
-			Msg("Using provided kcrypt config")
-	} else {
-		log.Logger.Info().Msg("No kcrypt config provided, using local encryption")
-	}
-
-	_, err = bus.Manager.Publish(bus.EventDiscoveryPassword, payload)
-	if err != nil {
-		log.Logger.Error().Err(err).Msg("Failed to publish event to bus")
-		return password, err
-	}
-
-	if password == "" {
-		log.Logger.Error().Msg("Received empty password from plugin")
-		return password, fmt.Errorf("received empty password")
-	}
-
-	log.Logger.Info().Msg("Password retrieval successful")
-	return
-}
 
 func luksUnlock(device, mapper, password string, logger *types.KairosLogger) error {
 	// Check if device exists and is accessible
