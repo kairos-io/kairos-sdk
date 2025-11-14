fe3902656d155317119136b0429df5ee3d0a7838
Itxaka <itxaka@kairos.io>
2025-11-14 15:16:41 +0100

    lint
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- types/spec/spec.go
+++ types/spec/spec.go
@@ -2,14 +2,14 @@ package spec
 
 import "github.com/kairos-io/kairos-sdk/types/partitions"
 
-// Spec is the interface that all specs need to implement
+// Spec is the interface that all specs need to implement.
 type Spec interface {
 	Sanitize() error
 	ShouldReboot() bool
 	ShouldShutdown() bool
 }
 
-// SharedInstallSpec is the interface that Install specs need to implement
+// SharedInstallSpec is the interface that Install specs need to implement.
 type SharedInstallSpec interface {
 	GetPartTable() string
 	GetTarget() string
