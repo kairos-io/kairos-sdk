9d65394a12db86d600f60775294e815877bfbb3e
Itxaka <itxaka@kairos.io>
2025-11-14 10:08:55 +0100

    feat: enhance partition and image structures
    - Updated Partition struct to include JSON serialization for all fields.
    - Introduced ElementalPartitions struct for better organization of partition types.
    - Added new Bundle and Image structs with appropriate serialization.
    - Created Install struct to centralize installation configuration options.
    
    These changes brings the commonly used structs in the install part of kairos-agent into the shared libs for ease of use by third parties, especiall by providers that want to sync to the structures we use in order to react to install events.
    
    Signed-off-by: Itxaka <itxaka@kairos.io>

---
+++ types/bundles.go
@@ -0,0 +1,11 @@
+package types
+
+type Bundles []Bundle
+
+type Bundle struct {
+	Repository string   `yaml:"repository,omitempty" json:"repository,omitempty" mapstructure:"repository"`
+	Rootfs     string   `yaml:"rootfs_path,omitempty" json:"rootfs_path,omitempty" mapstructure:"rootfs_path"`
+	DB         string   `yaml:"db_path,omitempty" json:"db_path,omitempty" mapstructure:"db_path"`
+	LocalFile  bool     `yaml:"local_file,omitempty" json:"local_file,omitempty" mapstructure:"local_file"`
+	Targets    []string `yaml:"targets,omitempty" json:"targets,omitempty" mapstructure:"targets"`
+}
--- types/partitions.go
+++ types/partitions.go
@@ -1,22 +1,32 @@
 package types
 
 type Partition struct {
-	Name            string   `yaml:"-"`
-	FilesystemLabel string   `yaml:"label,omitempty" mapstructure:"label"`
-	Size            uint     `yaml:"size,omitempty" mapstructure:"size"`
-	FS              string   `yaml:"fs,omitempty" mapstrcuture:"fs"`
-	Flags           []string `yaml:"flags,omitempty" mapstrcuture:"flags"`
-	UUID            string   `yaml:"uuid,omitempty" mapstructure:"uuid"`
-	MountPoint      string   `yaml:"-"`
-	Path            string   `yaml:"-"`
-	Disk            string   `yaml:"-"`
+	Name            string   `yaml:"name,omitempty" mapstructure:"name" json:"name,omitempty"`
+	FilesystemLabel string   `yaml:"label,omitempty" mapstructure:"label" json:"label,omitempty"`
+	Size            uint     `yaml:"size,omitempty" mapstructure:"size" json:"size,omitempty"`
+	FS              string   `yaml:"fs,omitempty" mapstrcuture:"fs" json:"fs,omitempty"`
+	Flags           []string `yaml:"flags,omitempty" mapstrcuture:"flags" json:"flags,omitempty"`
+	UUID            string   `yaml:"uuid,omitempty" mapstructure:"uuid" json:"uuid,omitempty"`
+	MountPoint      string   `yaml:"-" json:"-"` // MountPoint is not serialized
+	LoopDevice      string   `yaml:"-" json:"-"` // LoopDevice is not serialized
+	Path            string   `yaml:"-" json:"-"` // Path is not serialized
+	Disk            string   `yaml:"-" json:"-"` // Disk is not serialized
 }
 
 type PartitionList []*Partition
 
 type Disk struct {
-	Name       string        `json:"name,omitempty" yaml:"name,omitempty"`
-	SizeBytes  uint64        `json:"size_bytes,omitempty" yaml:"size_bytes,omitempty"`
-	UUID       string        `json:"uuid,omitempty" yaml:"uuid,omitempty"`
-	Partitions PartitionList `json:"partitions,omitempty" yaml:"partitions,omitempty"`
+	Name       string        `json:"name,omitempty" mapstructure:"name" yaml:"name,omitempty"`
+	SizeBytes  uint64        `json:"size_bytes,omitempty" mapstructure:"size_bytes" yaml:"size_bytes,omitempty"`
+	UUID       string        `json:"uuid,omitempty" mapstructure:"uuid" yaml:"uuid,omitempty"`
+	Partitions PartitionList `json:"partitions,omitempty" mapstructure:"partitions" yaml:"partitions,omitempty"`
+}
+
+type ElementalPartitions struct {
+	BIOS       *Partition `yaml:"-" json:"-"` // We dont want marshal/unmarshal this field
+	EFI        *Partition `yaml:"-" json:"-"` // We dont want marshal/unmarshal this field
+	OEM        *Partition `yaml:"oem,omitempty" mapstructure:"oem" json:"oem,omitempty"`
+	Recovery   *Partition `yaml:"recovery,omitempty" mapstructure:"recovery" json:"recovery,omitempty"`
+	State      *Partition `yaml:"state,omitempty" mapstructure:"state" json:"state,omitempty"`
+	Persistent *Partition `yaml:"persistent,omitempty" mapstructure:"persistent" json:"persistent,omitempty"`
 }
